3539e12b20076ee3adba896b3c98f89d7c378318
FMajewski <51134059+FMajewski@users.noreply.github.com>
2019-09-28 17:34:08 +0200

    yt video added, autoplay, blocked and full screen

--- app.js
+++ app.js
@@ -18,6 +18,68 @@ document.addEventListener("DOMContentLoaded", function(){
         menu.classList.toggle("show");
     })
 
+    $('.parallax-window').parallax({imageSrc: '/path/to/image.jpg'});
+
+
+
+    $(window).on('resize load', function() {
+        $('.site-header iframe').each(function() {
+            var self = $(this);
+            var container = self.parent();
+
+            self.css({
+                width: container.width() + "px",
+                height: container.width() * (8 /16) + 'px',
+                position: 'absolute',
+                marginTop: -container.width() * (9/32) + 'px',
+                top: '50%'
+            });
+        });
+    });
+
+
+// Load the IFrame Player API code asynchronously.
+    var tag = document.createElement('script');
+    tag.src = "https://www.youtube.com/player_api";
+    var firstScriptTag = document.getElementsByTagName('script')[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+``
+    var player;
+    function onYouTubePlayerAPIReady() {
+        player = new YT.Player('yt-player', {
+            autoplay : 1,
+            videoId: 'yZM5VAKi5vU',
+            playerVars : {
+                'autoplay' : 1,
+                'rel' : 0,
+                'showinfo' : 0,
+                'showsearch' : 0,
+                'controls' : 0,
+                'loop' : 1,
+                'enablejsapi' : 1,
+                'playlist': 'yZM5VAKi5vU'
+            },
+            events: {
+                "onReady": onPlayerReady,
+                //"onStateChange": onPlayerStateChange
+            }
+        });
+    }
+
+    function onPlayerReady(event) {
+        event.target.mute();
+        event.target.playVideo();
+    }
+
+    function onPlayerStateChange(event) {
+        var id = 'Ebyz8xxoK1w';
+
+        if(event.data === YT.PlayerState.ENDED){
+            player.loadVideoById(id);
+        }
+    }
+
+
 });
 
 function myFunction(x) {
